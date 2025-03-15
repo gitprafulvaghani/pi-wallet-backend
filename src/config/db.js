0aada46f1715a6ea09ea95859b527d402aaf09ea
Praful Vaghani <imac@Prafuls-MacBook-Air.local>
2025-03-15 15:03:10 +0530

    save local changes beforing pulling main

--- src/config/db.js
+++ src/config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
-
+//connect db
 const connectDB = async () => {
     try {
         await mongoose.connect(process.env.MONGO_URI, {
